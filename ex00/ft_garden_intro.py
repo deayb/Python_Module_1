daf7672a321fa13549d13b2ad5c45eb9db0a74a7
Deni Ayoubov <denayoub@c2r1p2.42nice.fr>
2026-05-05 16:04:56 +0200

    add

---
+++ ex00/ft_garden_intro.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+def main():
+    name = "Rose"
+    height = 56
+    age = 5
+    print("=== Welcome to my garden ===")
+    print(f"Plant name: {name}")
+    print(f"Height: {height} cm")
+    print(f"Age: {age} years")
+    print("=== End of program ===")
+
+
+if __name__ == "__main__":
+    main()
