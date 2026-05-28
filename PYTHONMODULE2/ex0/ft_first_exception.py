c7b865122fb1ed91036cefe2d15ca36e85030299
Deni Ayoubov <denayoub@c1r1p1.42nice.fr>
2026-05-28 22:42:50 +0200

    ex0

---
+++ PYTHONMODULE2/ex0/ft_first_exception.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+def input_temperature(temp_str):
+    return int(temp_str)
+
+
+def test_temperature():
+    print("=== Garden Temperature ===\n")
+    temp_str = "25"
+    print(f"Input data is '{temp_str}'")
+    temperature = input_temperature(temp_str)
+    print(f"Temperature is now {temperature}°C\n")
+    temp_str = "abc"
+    print(f"Input data is '{temp_str}'")
+    try:
+        temperature = input_temperature(temp_str)
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("All tests completed - program didn't crash!")
+
+
+test_temperature()
